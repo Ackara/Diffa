2a20b32b16aaae1f9c30782c07b2843354700d19
Ackara <ackara.dev@outlook.com>
2018-09-10 13:00:41 -0400

    Add project files.

---
+++ Diffa/FolderAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Acklann.Diffa
+{
+    [AttributeUsage((AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method), AllowMultiple = false, Inherited = true)]
+    public sealed class FolderAttribute : Attribute
+    {
+        public FolderAttribute(string path)
+        {
+            Location = path;
+        }
+
+        public readonly string Location;
+    }
+}
---
+++ Diffa/ReporterAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acklann.Diffa
+{
+    [AttributeUsage((AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method), AllowMultiple = false, Inherited = true)]
+    public sealed class ReporterAttribute : Attribute
+    {
+        public ReporterAttribute(Type reporterType)
+        {
+        }
+    }
+}
