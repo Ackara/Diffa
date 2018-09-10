d867d8bc1fff232f5062f2bfee8e7222a3dbcfc0
Ackara <ackara.dev@outlook.com>
2018-09-10 19:08:58 -0400

    Add test project

---
+++ tests/Diffa.MSTest/Tests/BinaryApproverTest.cs
@@ -0,0 +1,30 @@
+﻿using Acklann.Diffa.Reporters;
+using Acklann.Diffa.Resolution;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acklann.Diffa.Tests
+{
+    [TestClass]
+    public class BinaryApproverTest
+    {
+        [TestMethod]
+        public void Approve_should_verify_a_string()
+        {
+            // Arrange
+            var sut = new BinaryApprover();
+
+            // Act
+            
+
+            // Assert
+        }
+
+        
+    }
+}
---
+++ tests/Diffa.MSTest/Tests/StackTracePathResolverTest.cs
@@ -0,0 +1,40 @@
+﻿using Acklann.Diffa.Resolution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+
+namespace Acklann.Diffa.Tests
+{
+    [TestClass]
+    public class StackTracePathResolverTest
+    {
+        [TestMethod]
+        public void Should_return_this_projects_directory()
+        {
+            // Arrange
+            var sut = new StackTracePathResolver();
+
+            var cwd = AppDomain.CurrentDomain.BaseDirectory;
+            System.Diagnostics.Debug.WriteLine(cwd);
+
+            // Act
+            var result = PathResolverBase.GetProjectDirectory();
+
+            // Assert
+            result.ShouldBe(SampleFile.ProjectDirectory);
+        }
+
+        //[TestMethod]
+        public void GetExpectedResultPath()
+        {
+            // Arrange
+            var sut = new StackTracePathResolver();
+
+            // Act
+            var expectedPath = sut.GetExpectedResultPath();
+
+            // Assert
+            expectedPath.ShouldBe("");
+        }
+    }
+}
