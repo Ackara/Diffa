e36f7f2e1de8b94f1f2eb0c6105d69cdb4038feb
Ackara <ackara.dev@outlook.com>
2018-09-29 19:34:15 -0400

    Increment the version number to '0.1.2'.

--- src/Diffa/Properties/AssemblyInfo.cs
+++ src/Diffa/Properties/AssemblyInfo.cs
@@ -1,3 +1,11 @@
 ﻿using System;
 
 [assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle("Diffa")]
+[assembly: AssemblyProduct("Diffa")]
+[assembly: AssemblyCompany("Ackara")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("MIT License")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.2")]
